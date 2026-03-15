4b361852c732251ffe19912ee84427f865e7fdb3
jobinasharebot <5493jobina@sharebot.net>
2026-03-15 19:03:53 +0300

    апгрейднул на покоешном джаваскрипт 67

--- script.js
+++ script.js
@@ -21,29 +21,29 @@ one.addEventListener('click', function ones(){
     current=+1;
 });
 two.addEventListener('click', function twos(){
-    current=+1;
+    current=+2;
 });
 three.addEventListener('click', function threes(){
-    current=+1;
+    current=+3;
 });
 four.addEventListener('click', function fours(){
-    current=+1;
+    current=+4;
 });
 five.addEventListener('click', function fives(){
-    current=+1;
+    current=+5;
 });
 six.addEventListener('click', function sixs(){
-    current=+1;
+    current=+6;
 });
 seven.addEventListener('click', function sevens(){
-    current=+1;
+    current=+7;
 });
 eight.addEventListener('click', function eights(){
-    current=+1;
+    current=+8;
 });
 nine.addEventListener('click', function nines(){
-    current=+1;
+    current=+9;
 });
 zero.addEventListener('click', function zeros(){
-    current=+1;
+    current=+10;
 });
