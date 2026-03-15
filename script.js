99601393700ef22ed9b4ca7c5905205ea7b50c43
jobinasharebot <5493jobina@sharebot.net>
2026-03-15 18:58:40 +0300

    567876

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
 const calculate = document.getElementById('calculate');
 const current = document.getElementById('current');
 const controls = document.getElementById('controls');
+
+//current.textContent=1
+
+const one = current.querySelector('1');
+const two = current.querySelector('2');
+const three = current.querySelector('3');
+const four = current.querySelector('4');
+const five = current.querySelector('5');
+const six = current.querySelector('6');
+const seven = current.querySelector('7');
+const eight = current.querySelector('8');
+const nine = current.querySelector('9');
+const zero = current.querySelector('0');
+
+
+
+one.addEventListener('click', function one(){
+    current=+1;
+});
+two.addEventListener('click', function one(){
+    current=+1;
+});
+three.addEventListener('click', function one(){
+    current=+1;
+});
+four.addEventListener('click', function one(){
+    current=+1;
+});
+five.addEventListener('click', function one(){
+    current=+1;
+});
+six.addEventListener('click', function one(){
+    current=+1;
+});
+seven.addEventListener('click', function one(){
+    current=+1;
+});
+eight.addEventListener('click', function one(){
+    current=+1;
+});
+nine.addEventListener('click', function one(){
+    current=+1;
+});
+zero.addEventListener('click', function one(){
+    current=+1;
+});
