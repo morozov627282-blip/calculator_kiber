e644fa6825f441221766e560a5dd84a1d775a7d2
rustam <ru8tam@vk.com>
2026-03-29 18:50:37 +0300

    /8

--- script.js
+++ script.js
@@ -3,34 +3,34 @@ const current = document.getElementById('current');
 const controls = document.getElementById('controls');
 
 //циферки
-const one = current.querySelector('1');
-const two = current.querySelector('2');
-const three = current.querySelector('3');
-const four = current.querySelector('4');
-const five = current.querySelector('5');
-const six = current.querySelector('6');
-const seven = current.querySelector('7');
-const eight = current.querySelector('8');
-const nine = current.querySelector('9');
-const zero = current.querySelector('0');
+const one = controls.querySelector('1');
+const two = controls.querySelector('2');
+const three = controls.querySelector('3');
+const four = controls.querySelector('4');
+const five = controls.querySelector('5');
+const six = controls.querySelector('6');
+const seven = controls.querySelector('7');
+const eight = controls.querySelector('8');
+const nine = controls.querySelector('9');
+const zero = controls.querySelector('0');
 //кнопочки
-const AC = current.querySelector('AC');
-const backsp = current.querySelector('backsp');
-const percent = current.querySelector('%');
-const del = current.querySelector('/');
-const multiply = current.querySelector('*');
-const minus = current.querySelector('-');
-const plus = current.querySelector('+');
-const equal = current.querySelector('=');
-const radix = current.querySelector('√x');
-const degree = current.querySelector('x²')
+const AC = controls.querySelector('AC');
+const backsp = controls.querySelector('backsp');
+const percent = controls.querySelector('%');
+const del = controls.querySelector('/');
+const multiply = controls.querySelector('*');
+const minus = controls.querySelector('-');
+const plus = controls.querySelector('+');
+const equal = controls.querySelector('=');
+const radix = controls.querySelector('√x');
+const degree = controls.querySelector('x²')
 //при нажатии на цифры
 one.addEventListener('click', function ones(){
-    current.textContent = current.textContent*10 + 1;
+    if ope
     
 });
 two.addEventListener('click', function twos(){
-    current.textContent = current.textContent*10 + 2;
+
 });
 three.addEventListener('click', function threes(){
     current.textContent = current.textContent*10 + 3;
